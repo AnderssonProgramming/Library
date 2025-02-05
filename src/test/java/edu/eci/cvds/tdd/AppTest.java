ec8932420536db57181a4c0c7eba488020990bd4
santi-crown <samtigomusik@gmail.com>
2025-02-04 19:49:50 -0500

    Add first unit test

--- src/test/java/edu/eci/cvds/tdd/AppTest.java
+++ src/test/java/edu/eci/cvds/tdd/AppTest.java
@@ -1,7 +1,6 @@
 package edu.eci.cvds.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 /**
---
+++ src/test/java/edu/eci/cvds/tdd/LibraryTest.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.tdd;
+
+import org.junit.jupiter.api.Test;
+
+public class LibraryTest {
+    
+
+    @Test
+    public void shouldAddBook(){
+        
+    }
+}
