a741fb34e60b2f1d6cc6d7f273252c5ace64d30e
AnderECI <andersson.sanchez-m@mail.escuelaing.edu.co>
2025-02-05 11:24:17 -0500

    Loan and user tests

--- src/test/java/edu/eci/cvds/tdd/library/loan/LoanTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/loan/LoanTest.java
@@ -1,5 +1,52 @@
 package edu.eci.cvds.tdd.library.loan;
 
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.user.User;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
 public class LoanTest {
-    
+
+    @Test
+    public void testGetAndSetBook() {
+        Loan loan = new Loan();
+        Book book = new Book("Hola", "Pedro", "4781248");
+        loan.setBook(book);
+        assertEquals(book, loan.getBook());
+    }
+
+    @Test
+    public void testGetAndSetUser() {
+        Loan loan = new Loan();
+        User user = new User();
+        loan.setUser(user);
+        assertEquals(user, loan.getUser());
+    }
+
+    @Test
+    public void testGetAndSetLoanDate() {
+        Loan loan = new Loan();
+        LocalDateTime loanDate = LocalDateTime.now();
+        loan.setLoanDate(loanDate);
+        assertEquals(loanDate, loan.getLoanDate());
+    }
+
+    @Test
+    public void testGetAndSetStatus() {
+        Loan loan = new Loan();
+        LoanStatus status = LoanStatus.ACTIVE;
+        loan.setStatus(status);
+        assertEquals(status, loan.getStatus());
+    }
+
+    @Test
+    public void testGetAndSetReturnDate() {
+        Loan loan = new Loan();
+        LocalDateTime returnDate = LocalDateTime.now().plusDays(7);
+        loan.setReturnDate(returnDate);
+        assertEquals(returnDate, loan.getReturnDate());
+    }
 }
--- src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
@@ -1,5 +1,36 @@
 package edu.eci.cvds.tdd.library.user;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
-    
+
+    @Test
+    public void testGetName() {
+        User user = new User();
+        user.setName("John Doe");
+        assertEquals("John Doe", user.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        User user = new User();
+        user.setName("Jane Doe");
+        assertEquals("Jane Doe", user.getName());
+    }
+
+    @Test
+    public void testGetId() {
+        User user = new User();
+        user.setId("12345");
+        assertEquals("12345", user.getId());
+    }
+
+    @Test
+    public void testSetId() {
+        User user = new User();
+        user.setId("67890");
+        assertEquals("67890", user.getId());
+    }
 }
