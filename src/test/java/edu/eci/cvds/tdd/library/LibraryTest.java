bc84ff50d847de355e5e706c1463bd52c03f3105
santi-crown <samtigomusik@gmail.com>
2025-02-07 00:47:31 -0500

    Add setUp to LibraryTest

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -3,6 +3,7 @@ package edu.eci.cvds.tdd.library;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import edu.eci.cvds.tdd.library.book.Book;
@@ -11,11 +12,30 @@ import edu.eci.cvds.tdd.library.loan.LoanStatus;
 import edu.eci.cvds.tdd.library.user.User;
 
 public class LibraryTest {
+    private Library library;
+    private Book book;
+    private User user;
+    private Loan loan;
+
+    @BeforeEach
+    public void setUp() {
+        library = new Library();
+        
+        user = new User();
+        user.setName("Cris");        
+        user.setId("1001");
+
+        book = new Book("They prince", "Maquiavelo","1001");        
+
+        library.addBook(book);
+        library.addUser(user);  
+
+        loan = library.loanABook(user.getId(), book.getIsbn());  
+    }
+
     @Test
     public void testAddBookShouldTrue(){
         // arrange
-        Book book = new Book("They prince", "Maquiavelo","1001");
-        Library library = new Library();
 
         // act
         boolean result = library.addBook(book);
@@ -24,34 +44,10 @@ public class LibraryTest {
         assertTrue(result);
     }
 
-    // @Test
-    // public void testAddBookShouldFalse(){
-    //     // arrange
-    //     Book book = new Book("They prince", "Maquiavelo","1001");
-    //     Library library = new Library();
-
-    //     // act
-    //     boolean result = library.addBook(book);
-
-    //     // assert
-    //     assertFalse(result);
-    // }
-
     @Test
     public void testLoanABookShouldTrue() {
-        
-        Book book = new Book("The Prince", "Machiavelli", "1001");
-        User user = new User();
-        user.setName("Cris");
-        user.setId("1001");
-        Library library = new Library();
-        library.addBook(book);
-        library.addUser(user);  
 
-        
-        Loan loan = library.loanABook(user.getId(), book.getIsbn());  
-
-        
+    
         assertTrue(loan != null && loan.getStatus() == LoanStatus.ACTIVE);
     }
     
@@ -69,34 +65,8 @@ public class LibraryTest {
     // }
     
 
-    // private Library library;
-    // private Book book;
-    // private User user;
-    // private Loan loan;
-
-    // @Before
-    // public void setUp() {
-    //     library = new Library();
-    //     book = new Book("Effective Java", "Joshua Bloch", "1234567890");
-    //     user = new User("001", "John Doe");
-
-    //     library.addBook(book);
-    //     library.addUser(user);
-    //     loan = library.loanABook(user.getId(), book.getIsbn());
-    // }
-
     @Test
     public void testReturnLoanSuccessfully() {
-        Library library = new Library();
-        Book book = new Book("Effective Java", "Joshua Bloch", "1234567890");
-        User user = new User();
-        user.setName("Nick");
-        user.setId("01");
-
-        library.addBook(book);
-        library.addUser(user);
-        Loan loan = library.loanABook(user.getId(), book.getIsbn());
-
         Loan returnedLoan = library.returnLoan(loan);
 
         assertNotNull(returnedLoan);
